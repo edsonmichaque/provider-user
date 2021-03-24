6b203faf62d47b3b2e49a75b4c0455cd083a7e4a
Edson Michaque <edson@michaque.com>
2021-03-24 22:58:12 +0200

    Turn UserProvider into a function

--- user_provider.go
+++ user_provider.go
@@ -8,48 +8,33 @@ import (
 )
 
 const (
-	providerLevels = 2
-	providerLevelOperation = 1
-	providerLevelOS = 0
-	linuxProvider = "linux"
-	linuxCreateOperation = "create"
-	linuxUpdateOperation = "update"
-	linuxDeleteOperation = "delete"
+	depth = 2
+	operation = 1
+	os = 0
+	Linux = "linux"
+	Create = "create"
+	Update = "update"
+	Delete = "delete"
 )
 
-type UserProvider struct {
-}
-
-func New() *UserProvider {
-	return &UserProvider{}
-}
-
-func(u UserProvider) Name() string {
-	return "user"
-}
-
-func(u UserProvider) Provides() map[]string {
-	return []string{
-		"linux",
-	}
-}
-
-func(u UserProvider) Execute(levels ...string, args ...provider.Argument) (provider.Operation, error) {
-	if len(levels) != providerLevels {
-		return nil, errors.New("Must have 2 levels")
-	}
-
-	switch levels[providerLevelOS] {
-	case linuxProvider:
-		switch levels[providerLevelOperation] {
-		case linuxCreateOperation:
-			return linux.CreateUser(args...)
-		case linuxUpdateOperation:
-			return linux.UpdateUser(args...)
-		case linuxDeleteOperation:
-			return linux.DeleteUser(args...)
+func NewProvider() sdk.Provider {
+	return func(levels ...string, args ...provider.Argument) (provider.Operation, error) {
+		if len(levels) != depth {
+			return nil, errors.New("Must have 2 levels")
 		}
-	}
 
-	return nil, errors.New("Cannot find provider")
+		switch levels[os] {
+		case Linux:
+			switch levels[operation] {
+			case Create:
+				return linux.CreateUser(args...)
+			case Update:
+				return linux.UpdateUser(args...)
+			case Delete:
+				return linux.DeleteUser(args...)
+			}
+		}
+
+		return nil, errors.New("Cannot find provider")
+	}
 }
